74d11bc689757b62e47a4cf425eaae7129029433
kacy2233 <mi994135@163.com>
2023-12-20 19:57:01 +0800

    Site updated: 2023-12-20 19:57:01

--- js/runtime.js
+++ js/runtime.js
@@ -19,7 +19,7 @@ function createtime() {
   let c = "";
   (c =
     s < 18 && s >= 9
-      ? `<img class='boardsign' src='https://picbed.em1ra.top/img/alpha-e-box.svg' title='什么时候能够实现财富自由呀~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>`
+      ? `<img class='boardsign' src='https://picbed.em1ra.top/img/alpha-e-box.svg' title='什么时候能够实现财富自由呀~'width="10"><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>`
       : `<img class='boardsign' src='https://picbed.em1ra.top/img/alpha-e-box.svg' title='下班了就该开开心心地玩耍~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>`),
     document.getElementById("workboard") &&
       (document.getElementById("workboard").innerHTML = c);
